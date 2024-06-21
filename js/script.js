123b28b58c90e47b1bb63f32f8ea0b532a3943f4
Luka <luka.luka.luka93@gmail.com>
2024-06-21 12:46:53 -0700

    added click button to the qr code input with a function that creates new qr code each time we paste a link to the input, closes the first container, opens the second one prompted with the picture of a new qr code

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ console.log(qrImg)
 
 
 
+handleClick = (e) => {
+    e.preventDefault()
+    const inputValue = qrInput.value;
+    qrImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+    container.classList.add("hide");
+    container2.classList.remove("hide");
+}
+
+qrButton.addEventListener("click", handleClick);
