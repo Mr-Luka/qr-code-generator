f877a103db08d21a1dc3cadbebe82f2d60b2a4e5
Luka <luka.luka.luka93@gmail.com>
2024-06-21 12:45:52 -0700

    selected all the necessary elements

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const qrButton = document.querySelector("button.qr-code-button");
+const qrInput = document.querySelector("#input-text");
+const qr = document.querySelector("div.qr");
+const qrImg = document.querySelector("#qr-code");
+const container = document.querySelector(".container");
+const container2 = document.querySelector(".second-page");
+console.log(qrImg)
+
+
+
+
