e004535cf76e4ca6651d74cb87abf858be1003ba
Luka <luka.luka.luka93@gmail.com>
2024-06-24 13:53:58 -0700

    fixed handle click

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const qrButton = document.querySelector("button.qr-code-button");
 const qrInput = document.querySelector("#input-text");
-const qr = document.querySelector("div.qr");
 const qrImg = document.querySelector("#qr-code");
 const container = document.querySelector(".container");
 const container2 = document.querySelector(".second-page");
-console.log(qrImg)
+const download = document.querySelector("[data-download]");
+const share = document.querySelector("#share");
+console.log(download)
+
 
 
 
@@ -12,9 +14,31 @@ console.log(qrImg)
 handleClick = (e) => {
     e.preventDefault()
     const inputValue = qrInput.value;
+    if (inputValue) {
     qrImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
     container.classList.add("hide");
     container2.classList.remove("hide");
+    } else {
+        alert("Please enter a URL or text")
+    }
 }
 
 qrButton.addEventListener("click", handleClick);
+
+
+handleDownload = async (e) => {
+    e.preventDefault();
+    await fetchFile(qrImg.src);
+}
+async function fetchFile (url) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const urlObject = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = urlObject;
+    a.download = 'qr-code.png';
+    document.body.appendChild(a);
+    URL.revokeObjectURL(urlObject);
+}
+
+download.addEventListener("click", handleDownload);
